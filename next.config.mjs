4ab560325ff7e4d49ee371cf923b2e06a7c4bb0e
thanh0092 <thanh.bui@tomosia.com>
2024-05-13 15:02:07 +0700

    feat: :art: baseFlocss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
